be2762d4cd1a41db17fa3dfe2a8b9a813575ee9c
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-02-22 15:57:39 +0300

    added custom alert controller UI

---
+++ Movie Searcher/Movie Searcher/Views/CustomAlertViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomAlertViewController.swift
+//  Movie Searcher
+//
+//  Created by Bartuğ Kaan Çelebi on 22.02.2024.
+//
+
+import UIKit
+
+class CustomAlertViewController: UIViewController {
+
+  @IBOutlet var alertImage: UIImageView!
+  @IBOutlet var alertTitle: UILabel!
+  @IBOutlet var alertMessage: UILabel!
+  @IBOutlet var alerButton: UIButton!
+  
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+  
+  @IBAction func buttonTapped(_ sender: UIButton) {
+  }
+  
+}
